922e491eb0fa78bbc1e55ca153bd6ea61aa3e023
loanyihshiou <loanyihshiou@gmail.com>
2020-01-06 17:58:34 +0800

    update idriplogin method

--- iDrip.py
+++ iDrip.py
@@ -9,6 +9,7 @@ import os
 from Idrip_login import IdripLogin
 
 
+
 class IdripTest(unittest.TestCase):
 
     def setUp(self):
@@ -26,9 +27,8 @@ class IdripTest(unittest.TestCase):
         """同意權限"""
         self.idrip.find_element(By.ID, "com.coffee.iDrip:id/textView_yes").click()
         time.sleep(1)
-        self.idrip.find_element_by_id("com.android.packageinstaller:id/permission_allow_button").click()
-        self.idrip.find_element_by_id("com.android.packageinstaller:id/permission_allow_button").click()
-        self.idrip.find_element_by_id("com.android.packageinstaller:id/permission_allow_button").click()
+        for i in range(3):
+            self.idrip.find_element_by_id("com.android.packageinstaller:id/permission_allow_button").click()
 
     def tearDown(self):
         self.idrip.quit()
@@ -36,12 +36,13 @@ class IdripTest(unittest.TestCase):
 
     # @unittest.skip
     def test01_shopping_cart(self):
-        id.login("louistest0625@gmail.com", 711228)
+        page = IdripLogin(self.idrip)
+        page.login("louistest0625@gmail.com", 711228)
         self.idrip.find_element_by_id("com.coffee.iDrip:id/view_salon").click()
         self.idrip.find_element_by_id("com.coffee.iDrip:id/asyncImageGlideView").click()
         # 下滑三次
         for i in range(3):
-            self.swipe_up(1000)
+            page.swipe_up(1000)
         self.idrip.find_element_by_id("com.coffee.iDrip:id/view_add").click()
         time.sleep(2)
         for i in range(2):
@@ -54,8 +55,8 @@ class IdripTest(unittest.TestCase):
 
     # @unittest.skip
     def test02_market(self):
-        idrip = IdripLogin()
-        idrip.login("louistest0625@gmail.com", 711228)
+        page = IdripLogin(self.idrip)
+        page.login("louistest0625@gmail.com", 711228)
         self.idrip.find_element_by_id("com.coffee.iDrip:id/linearLayout_shop").click()
         self.idrip.find_element_by_id("com.coffee.iDrip:id/textView_subtitle").click()
         self.idrip.find_element_by_id("com.coffee.iDrip:id/button").click()
@@ -63,16 +64,17 @@ class IdripTest(unittest.TestCase):
         self.idrip.find_element_by_id("com.coffee.iDrip:id/view_add").click()
         self.idrip.find_element_by_id("com.coffee.iDrip:id/button_ok").click()
         time.sleep(0.5)
-        toast = self.is_toast_exist("購物車完成")
+        toast = page.is_toast_exist("購物車完成")
         self.assertEqual(toast, True)
 
     # @unittest.skip
     def test03_market(self):
-        self.login("louistest0625@gmail.com", 711228)
+        page = IdripLogin(self.idrip)
+        page.login("louistest0625@gmail.com", 711228)
         self.idrip.find_element_by_xpath("//android.widget.TextView[@text='咖啡市集']").click()
         self.idrip.find_element_by_xpath(
             "//android.widget.TextView[contains(@resource-id, 'packet') and @text='咖啡包']").click()
-        self.swipe_up(500)
+        page.swipe_up(500)
         self.idrip.find_element_by_xpath("//android.widget.RelativeLayout[@index='2']").click()
         self.idrip.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/"
                                          "android.widget.FrameLayout/android.widget.LinearLayout/android.widget"
@@ -80,10 +82,9 @@ class IdripTest(unittest.TestCase):
                                          ".widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget"
                                          ".RelativeLayout[2]/android.view.View[1]").click()
         self.idrip.find_element_by_id("com.coffee.iDrip:id/button_ok").click()
-        toast = self.is_toast_exist("加入購物車完成")
+        toast = page.is_toast_exist("加入購物車完成")
         self.assertEqual(toast, True)
 
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
-
