50a4ce206b78a6f3be72b65c0fc8981d5140faab
loanyihshiou <loanyihshiou@gmail.com>
2020-01-06 17:18:37 +0800

    Update idrip_login.py

--- idrip_login.py
+++ idrip_login.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
@@ -9,6 +8,7 @@ class IdripLogin:
     def __init__(self, idrip):
         self.idrip = idrip
 
+
     def login(self, username, password):
         """登入"""
         self.idrip.find_element_by_id("com.coffee.iDrip:id/linearLayout_my").click()
@@ -18,6 +18,27 @@ class IdripLogin:
         self.idrip.find_element_by_id("com.coffee.iDrip:id/editText_pw").send_keys(password)
         self.idrip.find_element_by_id("com.coffee.iDrip:id/linearLayout_next").click()
         self.idrip.find_element_by_id("com.coffee.iDrip:id/textview_skip").click()
+
+    def swipe_up(self, t):
+        x = self.idrip.get_window_size()['width']
+        y = self.idrip.get_window_size()['height']
+
+        s = [x, y]
+        sx1 = int(s[0] * 0.5)  # 起始x座標
+        sy1 = int(s[1] * 0.75)  # 起始y座標
+        sy2 = int(s[1] * 0.25)  # 終點y座標
+        time.sleep(1)
+        self.idrip.swipe(sx1, sy1, sx1, sy2, t)
+        time.sleep(2)
+
+    def is_toast_exist(self, text):
+        try:
+            toast_loc = ("xpath", ".//*[contains(@text,'%s')]" % text)
+            WebDriverWait(self.idrip, 10, 0.05).until(EC.presence_of_element_located(toast_loc))
+            print(text)
+            return True
+        except:
+            return False
 
 
 if __name__ == "__main__":
